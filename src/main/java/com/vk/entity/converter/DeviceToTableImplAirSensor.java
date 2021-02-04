aa11528719076b1f32b7e32ae9cf380815767f0e
KIP-PC99 <vlad.mishaniy@gmail.com>
2021-02-04 12:31:39 +0200

    add new sensor for air conditions

--- src/main/java/com/vk/entity/converter/DeviceToTableImplAirSensor.java
+++ src/main/java/com/vk/entity/converter/DeviceToTableImplAirSensor.java
@@ -27,7 +27,7 @@ public class DeviceToTableImplAirSensor implements DeviceToTableAirSensor{
                     case "SDS_P2": tableModel.setSDS_P2(Float.parseFloat(sensor.getValue())); break;
                     case "BME280_temperature": tableModel.setBME280_temperature(Float.parseFloat(sensor.getValue())); break;
                     case "BME280_humidity": tableModel.setBME280_humidity(Float.parseFloat(sensor.getValue())); break;
-                    case "BME280_pressure": tableModel.setBME280_pressure(Float.parseFloat(sensor.getValue())); break;
+                    case "BME280_pressure": tableModel.setBME280_pressure(Float.parseFloat(sensor.getValue())/100F); break;
                     case "NH3": tableModel.setNH3(Float.parseFloat(sensor.getValue())); break;
                     case "CO": tableModel.setCO(Float.parseFloat(sensor.getValue())); break;
                     case "NO2": tableModel.setNO2(Float.parseFloat(sensor.getValue())); break;
